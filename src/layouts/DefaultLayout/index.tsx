1eb64db141567caab059c4568b1ee196821d50f5
Jean Santos <jeansantos.sw@gmail.com>
2023-02-07 14:43:23 +0000

    Configurando react router

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom'
 import { Header } from '../../components/Header'
 import { LayoutContainer } from './styles'
 
-export function Router() {
+export function DefaultLayout() {
   return (
     <LayoutContainer>
       <Header />
