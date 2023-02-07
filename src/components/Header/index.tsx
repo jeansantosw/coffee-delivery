a6adb6a379ca85344a04ecc2cb5e574109570cf6
Jean Santos <jeansantos.sw@gmail.com>
2023-02-07 15:33:28 +0000

    Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,27 @@
+import { MapPin, ShoppingCart } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
+import logoCoffeeDelivery from '../../assets/logo-coffee-delivery.svg'
+
+import { HeaderButton, HeaderButtonsContainer, HeaderContainer } from './styles'
+
 export function Header() {
-  return <h1>Header</h1>
+  return (
+    <HeaderContainer>
+      <div className="container">
+        <img src={logoCoffeeDelivery} alt="" />
+        <HeaderButtonsContainer>
+          <HeaderButton variant="purple">
+            <MapPin size={20} />
+            Manaus - AM
+          </HeaderButton>
+          <NavLink to="#">
+            <HeaderButton variant="yellow">
+              <span>0</span>
+              <ShoppingCart size={20} />
+            </HeaderButton>
+          </NavLink>
+        </HeaderButtonsContainer>
+      </div>
+    </HeaderContainer>
+  )
 }
